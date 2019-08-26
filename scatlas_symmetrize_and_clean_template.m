308459764672985cfd3deed0c5caa4cdff58ed33
Julien Cohen-Adad <jcohen@polymtl.ca>
2019-08-26 15:01:50 -0400

    scatlas_symmetrize_and_clean_template.m: Improved symmetrization - added WM and cord mask - better generation of mask for cleaning

--- scatlas_symmetrize_and_clean_template.m
+++ scatlas_symmetrize_and_clean_template.m
@@ -20,7 +20,7 @@ clear
 % load params
 run scatlas_parameters.m
 
-fprintf('====================\nSYMMETRIZE AND CLEAN\n====================\n')
+fprintf('====================\nSYMMETRIZE AND CLEAN\n====================')
 
 % go to data folder
 cd(fullfile(PATH_DATA, FOLDER_LEVELS))
@@ -29,15 +29,18 @@ cd(fullfile(PATH_DATA, FOLDER_LEVELS))
 
 % loop over levels
 for level=1:length(LIST_LEVELS)
-    
+    fprintf('\nProcessing level %s (%i/%i)...', LIST_LEVELS{level}, level, length(LIST_LEVELS));
     cd(LIST_LEVELS{level});
 
     % Part 1: symmetrize by using the flips
+    % =====================================
+    
     % Load template and the std
     img=load_nii_data('Volume4D.nii.gz');
     img_std=load_nii_data('Volume4D_std.nii.gz');
+    img_mask_wm = load_nii_data('mask_WM.nii.gz');
 
-    % for each level
+    % for each metric
     for i=1:size(img,4)
     
         map=img(:,:,1,i);
@@ -49,25 +52,31 @@ for level=1:length(LIST_LEVELS)
         img_std(:,:,1,i)=(map_std+map_flipped_std)./2;
 
     end
+    % WM mask
+    img_mask_wm_sym = (img_mask_wm+flip(img_mask_wm))./2;
+    % binarize using mid threshold
+    img_mask_wm_sym_bin = imbinarize(img_mask_wm_sym, max(max(img_mask_wm_sym))/2);
+    % create cord mask  (fill hole)
+    img_mask_cord = imfill(img_mask_wm_sym_bin,'holes');
 
-    % Save symmetrized template and std
+    % Save symmetrized data
     save_nii_v2(img,'Volume4D_sym.nii.gz','Volume4D.nii.gz',16);
     save_nii_v2(img_std,'Volume4D_sym_std.nii.gz','Volume4D_std.nii.gz',16);
+    save_nii_v2(img_mask_wm_sym_bin,'mask_WM_sym.nii.gz','mask_WM.nii.gz',2);
+    save_nii_v2(img_mask_cord,'mask_cord_sym.nii.gz','mask_WM.nii.gz',2);
 
+    
     % Part 2: clean template
+    % ======================
+    
     % load volume of concatenated metrics template
     Volume4D=load_nii_data('Volume4D_sym.nii.gz');
     Volume4D_std=load_nii_data('Volume4D_sym_std.nii.gz');
 
-    % load mask template output to filter maps (white + gray matter)
-    mask=load_nii_data('templategen/template/T_template0.nii.gz');
-    mask=im2bw(mask);
-    mask=imfill(mask,'holes');
-    
-    % clean maps and stds
+    % clean maps and stds using cord mask
     for ooo=1:size(Volume4D,4)
-        Volume4D(:,:,1,ooo)=Volume4D(:,:,1,ooo).*mask;
-        Volume4D_std(:,:,1,ooo)=Volume4D_std(:,:,1,ooo).*mask;
+        Volume4D(:,:,1,ooo)=Volume4D(:,:,1,ooo) .* img_mask_cord;
+        Volume4D_std(:,:,1,ooo)=Volume4D_std(:,:,1,ooo) .* img_mask_cord;
     end
     
     % Save cleaned template and std

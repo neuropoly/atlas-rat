e90e0a25a2b0cce11bc313a7983c63840673c928
Julien Cohen-Adad <jcohen@polymtl.ca>
2019-08-28 12:05:01 -0400

    scatlas_create_spinal_levels: New script to generate spinal levels

---
+++ scatlas_create_spinal_levels.m
@@ -0,0 +1,40 @@
+%--------------------------------------------------------------------------
+% Create 3D file of spinal levels
+%--------------------------------------------------------------------------
+
+clear
+
+% load params
+run scatlas_parameters.m
+
+fprintf('=========================\nCREATE SPINAL LEVELS\n=========================')
+
+cd(PATH_DATA);
+
+% go to data folder
+cd(fullfile(PATH_DATA, FOLDER_LEVELS))
+
+% Load spinal cord mask
+mask_cord = load_nii_data('AtlasRat_mask_cord.nii.gz');
+
+% Initialize 3D empty object
+spinal_levels = zeros(size(mask_cord));
+nz = size(spinal_levels, 3);
+% loop over slice and assign incremental value for each spinal level
+for iz=1:nz
+
+    % Replace all non-null pixel with value iz
+    ind_nonzero = find(mask_cord(:, :, iz));
+    spinal_levels_slice = zeros(size(mask_cord, 1), size(mask_cord, 2));
+    spinal_levels_slice(ind_nonzero) = iz;
+    % starts with the last slice (which corresponds to the most rostral).
+    spinal_levels(:, :, nz-iz+1) = spinal_levels_slice;
+
+end
+
+% save final volume in nii
+fprintf('\nSave file...');
+save_nii_v2(spinal_levels,'AtlasRat_spinal_levels.nii.gz','AtlasRat_mask_cord.nii.gz', 4);
+
+fprintf('\nDone!\n');
+
